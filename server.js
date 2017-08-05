e4033c47202a2af39b8cd3af0eeb3ce3f631076e
yplsingh <panna.yadav@gmail.com>
2017-08-05 15:05:21 +0530

    Changes around article exercise.

--- server.js
+++ server.js
@@ -5,6 +5,80 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+   'article-one' : {
+      title : 'Article One | Panna Yadav',
+      heading : 'Article One',
+      date: 'Aug 5, 2017',
+      content:`<p>
+        This is the content of my first article. This is the content of my first article.
+        This is the content of my first article. This is the content of my first article.
+        This is the content of my first article. This is the content of my first article.
+      </p>
+      <p>
+          This is the content of my first article. This is the content of my first article.
+          This is the content of my first article. This is the content of my first article.
+          This is the content of my first article. This is the content of my first article.
+          This is the content of my first article. This is the content of my first article.
+        </p>
+        <p>
+          This is the content of my first article. This is the content of my first article.
+          This is the content of my first article. This is the content of my first article.
+          This is the content of my first article. This is the content of my first article.
+          This is the content of my first article. This is the content of my first article.
+        </p>`
+      },
+      'article-two' : {
+         title : 'Article Two | Panna Yadav',
+         heading : 'Article Two',
+         date: 'Aug 10, 2017',
+         content:`<p>
+           This is the content of my second article. This is the content of my second article.
+         </p>
+         `
+       },
+       'article-three' : {
+          title : 'Article Three | Panna Yadav',
+          heading : 'Article Three',
+          date: 'Aug 15, 2017',
+          content:`<p>
+            This is the content of my third article. This is the content of my third article.
+          </p>
+          `
+          }
+};
+
+function createTemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `
+                  <html>
+                    <head>
+                      <title>${title}</title>
+                      <meta name="viewport" content="width=device-width, initial-scale=1">
+                      <link href="/ui/style.css" rel="stylesheet" />
+                    </head>
+                    <body>
+                      <div class="container">
+                        <div>
+                        <a href="/">Home</a>
+                        </div>
+                        <hr/>
+                        <h3> ${heading} </h3>
+                        <div>
+                            ${date}
+                        </div>
+                        <div>
+                          ${content}
+                        </div>
+                      </div>
+                    </body>
+                  </html>
+                  `;
+      return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,10 +87,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/panna.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'panna.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
