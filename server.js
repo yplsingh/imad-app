032f99bbfea4de6c2d6a74318892b81817747767
yplsingh <panna.yadav@gmail.com>
2017-08-08 00:27:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/test-db', function(req, res){
             res.status(500).send(err.toString());
         }
         else{
-            res.send(JSON.stringify(result.rows));
+            res.send(JSON.stringify(result.rows[0]));
         }
     });
 });
