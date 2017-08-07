2538f7e58f83ab792ffcf0e815a530a019c5a274
yplsingh <panna.yadav@gmail.com>
2017-08-08 00:14:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('combined'));
 var config = {
     user: 'pannayadav',
     database: 'pannayadav',
-    host: 'http://db.imad.hasura-app.io',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
 };
